afa8b7cc739174e1ff46128fcd3c04ca64007f91
Katie Hom <schaka01@gmail.com>
2022-09-21 09:29:49 -0600

    Added confirm delete modal. Added account route, controller, methods and view - commented out until finalized.

--- routes/todos.js
+++ routes/todos.js
@@ -4,15 +4,10 @@ const todosController = require('../controllers/todos')
 const { ensureAuth } = require('../middleware/auth')
 
 router.get('/', ensureAuth, todosController.getTodos)
-
 router.get('/add', ensureAuth, todosController.add)
-
 router.get('/edit/:id', ensureAuth, todosController.edit)
-
 router.put('/:id', ensureAuth, todosController.update)
-
 router.post('/createTodo', todosController.createTodo)
-
 router.delete('/deleteTodo/:id', todosController.deleteTodo)
 
 module.exports = router
