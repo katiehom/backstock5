5b386c7a0367fe33214bff70c670f4070bbd7180
Katie Hom <schaka01@gmail.com>
2022-09-23 13:43:23 -0600

    Changed tableSort to allow for date conversion and sorting

--- public/js/tablesort.js
+++ public/js/tablesort.js
@@ -4,7 +4,7 @@
 function sortTableByColumn(table, column, asc = true) {
     const dirModifier = asc ? 1 : -1
     const tBody = table.tBodies[0]
-    const date = Array.from(tBody.querySelectorAll('.expiration'))
+    // const date = Array.from(tBody.querySelectorAll('.expiration'))
     const rows = Array.from(tBody.querySelectorAll('tr'))
 
     // Sort each row
@@ -13,6 +13,8 @@ function sortTableByColumn(table, column, asc = true) {
         const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim()
         if (isNaN(parseFloat(aColText)) && isNaN(parseFloat(bColText))) {
             return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+        } else if ((aColText.includes('/') && aColText.includes('20')) && (bColText.includes('/') && bColText.includes('20'))) {
+            return new Date(aColText).getTime() > new Date(bColText).getTime() ? (1 * dirModifier) : (-1 * dirModifier)
         }
           return +aColText > +bColText ? (1 * dirModifier) : (-1 * dirModifier);
         });
@@ -44,9 +46,3 @@ document.querySelectorAll('.table-sortable th.sort').forEach(headerCell => {
     })
 })
 
-// document.querySelector('.expirationSort').addEventListener('click', datesSorter)
-// function datesSorter(a, b) {
-//     if (new Date(a) < new Date(b)) return -1
-//     if (new Date(a) > new Date(b)) return 1
-//     return 0
-// }
